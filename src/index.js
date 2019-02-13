181209ef2d8d45698874917412c312ede3fa1cd6
Ismael Ramos <iramosmerlos@outlook.com>
2019-02-13 00:05:07 -0600

    Add helps to secure app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 
 // Expose API REST
